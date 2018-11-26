6d19b755624cf888a595e4815951f1a7a73a7ec7
kush <ryan.kush@garmin.com>
2018-11-25 22:38:37 -0600

    Initial Commit
    Initial commit that creates the first initial project file for Project
    One.

---
+++ Project1-StormViewer/Project1-StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1-StormViewer
+//
+//  Created by Kush, Ryan on 11/25/18.
+//  Copyright © 2018 Kush, Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
