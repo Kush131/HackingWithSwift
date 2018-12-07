d3da9999e4be7f778ed8118b9978d672e9efa4c8
kush <ryankush131@gmail.com>
2018-12-06 21:54:40 -0600

    Create project five
    Create project five. Created a lot of the initial view controller
    logic, but stopped right as we were getting into closure syntax.

---
+++ Project5-WordScramble/Project5-WordScramble/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Project5-WordScramble
+//
+//  Created by Kush, Ryan on 12/6/18.
+//  Copyright © 2018 Kush, Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var allWords: [String] = []
+    var usedWords: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+            else {
+                allWords = ["silkworm"]
+            }
+        }
+        startGame()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { (action: UIAlertAction) in
+            let answer = ac.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+
+}
+
