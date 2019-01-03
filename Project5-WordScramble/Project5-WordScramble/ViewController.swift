adc5360c9bc443038a1ea7dc0d1f7d5cbb528ea9
kush <ryankush131@gmail.com>
2019-01-02 22:41:29 -0600

    Finish project five
    Finished off project five. Learned a lot about closures along
    with a few unique swift features (unowned/weak, try, capturing
    within closures to name a few).

--- Project5-WordScramble/Project5-WordScramble/ViewController.swift
+++ Project5-WordScramble/Project5-WordScramble/ViewController.swift
@@ -18,14 +18,7 @@ class ViewController: UITableViewController {
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
 
-        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
-            if let startWords = try? String(contentsOfFile: startWordsPath) {
-                allWords = startWords.components(separatedBy: "\n")
-            }
-            else {
-                allWords = ["silkworm"]
-            }
-        }
+        loadDefaultWords()
         startGame()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -40,7 +33,7 @@ class ViewController: UITableViewController {
         let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
 
-        let submitAction = UIAlertAction(title: "Submit", style: .default) { (action: UIAlertAction) in
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] (action: UIAlertAction) in
             let answer = ac.textFields![0]
             self.submit(answer: answer.text!)
         }
@@ -58,5 +51,78 @@ class ViewController: UITableViewController {
         return cell
     }
 
+    func submit(answer: String) {
+        let lowerAnswer = answer.lowercased()
+
+        guard title?.lowercased() != lowerAnswer else {
+            showErrorMessage(title: "Used same word", message: "You used the same word we're playing the game with!")
+            return
+        }
+
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    return
+                }
+                else {
+                    showErrorMessage(title: "Word not recognized", message: "You can't just make up words!")
+                }
+            }
+            else {
+                showErrorMessage(title: "Word already used", message: "Be more original!")
+            }
+        }
+        else {
+            showErrorMessage(title: "Word not possible", message: "You can't spell that word from '\(title!.lowercased())'!")
+        }
+
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord = title!.lowercased()
+        for letter in word {
+            if let pos = tempWord.range(of: String(letter)) {
+                tempWord.remove(at: pos.lowerBound)
+            }
+            else {
+                return false
+            }
+        }
+        return true
+    }
+
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+
+    func isReal(word: String) -> Bool {
+        guard word.utf16.count < 3 else {
+            return false
+        }
+        let checker = UITextChecker()
+        let range = NSMakeRange(0, word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+
+    func showErrorMessage(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+    }
+
+    func loadDefaultWords() {
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+                return
+            }
+        }
+        // We failed to do something, so just default to the "error list"
+        allWords = ["silkworm"]
+    }
 }
 
