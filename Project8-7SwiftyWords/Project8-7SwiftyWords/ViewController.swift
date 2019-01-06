08e2cb0d3512aed526ce00b40b29168c6d53647a
kush <ryankush131@gmail.com>
2019-01-05 23:53:43 -0600

    Create project 8 UI
    Created the project 8 UI. The coding portion will come later,
    but this UI was pretty tedious (but easy) to make.

---
+++ Project8-7SwiftyWords/Project8-7SwiftyWords/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Project8-7SwiftyWords
+//
+//  Created by Kush, Ryan on 1/5/19.
+//  Copyright © 2019 Kush, Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func submitTapped(_ sender: UIButton) {
+    }
+
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
+    
+
+}
+
