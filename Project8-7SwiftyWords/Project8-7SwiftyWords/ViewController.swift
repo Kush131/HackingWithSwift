d851c0b12baca1ae51fb426f043f7181ea2dd932
kush <ryankush131@gmail.com>
2019-01-06 21:37:18 -0600

    Complete Project 8
    Completed project 8 coding portion. The UI seems to be not
    very well thought out (text sometimes overlaps), but I probably
    would fix it by throwing each clue/letter count into its own
    label inside of a horizontal stack view. Then I would throw
    every stack view into a vertical stack view.
    
    Aside from the UI, I learned about the
    components(seperatedBy:)
    method and the joined(seperatedBy:) method. I also got a
    review of the Bundle.main.path(forResource:) method along
    with loading a file into a string with String(contentsOfFile:).
    Finally, I also learned about the addTarget() method that
    allows you to do the storyboard “control drag” in code.

--- Project8-7SwiftyWords/Project8-7SwiftyWords/ViewController.swift
+++ Project8-7SwiftyWords/Project8-7SwiftyWords/ViewController.swift
@@ -15,19 +15,112 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentAnswer: UITextField!
     @IBOutlet weak var scoreLabel: UILabel!
 
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
 
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var level = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        }
+        loadLevel()
     }
 
     @IBAction func submitTapped(_ sender: UIButton) {
+        if let solutionPosition = solutions.index(of: currentAnswer.text!) {
+            activatedButtons.removeAll()
+
+            var splitAnswers = answersLabel.text!.components(separatedBy: "\n")
+            splitAnswers[solutionPosition] = currentAnswer.text!
+            answersLabel.text = splitAnswers.joined(separator: "\n")
+
+            currentAnswer.text = ""
+            score += 1
+
+            if score % 7 == 0 {
+                let ac = UIAlertController(title: "Well Done!", message: "Are you ready for the next level?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Let's Go!", style: .default, handler: levelUp))
+                present(ac, animated: true)
+            }
+        }
     }
 
     @IBAction func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
+
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+        activatedButtons.removeAll()
     }
-    
+
+    @objc func letterTapped(btn: UIButton) {
+        if currentAnswer.text! == "Tap letters to guess" {
+            currentAnswer.text! = ""
+        }
+        currentAnswer.text = currentAnswer.text! + btn.titleLabel!.text!
+        activatedButtons.append(btn)
+        btn.isHidden = true
+    }
+
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+
+        if let levelFilePath = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
+            if let levelContents = try? String(contentsOfFile: levelFilePath) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines.shuffle()
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    clueString += "\(index + 1). \(clue)\n"
+
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+        // Configure buttons and labels
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        letterBits.shuffle()
+
+        if letterBits.count == letterButtons.count {
+            for i in 0..<letterButtons.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
+    }
+
+    func levelUp(action: UIAlertAction) {
+        level += 1
+        solutions.removeAll(keepingCapacity: true)
+
+        loadLevel()
+
+        for btn in letterButtons {
+            btn.isHidden = false
+        }
+    }
 
 }
 
