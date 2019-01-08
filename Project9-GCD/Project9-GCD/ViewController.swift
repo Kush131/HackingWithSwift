e0796523c5780bd13f1850ec556617a4707abc3e
kush <ryankush131@gmail.com>
2019-01-07 21:17:36 -0600

    Complete Project 9
    Complete project 9, which augmented project 7 with GCD. At
    first, we went over DispatchQueue and how you can use
    various quality of servies on a global background thread to
    prioritize work. Later on we implemented the performSelector
    method that allowed us to not have to worry about strong
    reference cycles or closures.

---
+++ Project9-GCD/Project9-GCD/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Project9-GCD
+//
+//  Created by Kush, Ryan on 1/7/19.
+//  Copyright © 2019 Kush, Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var petitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
+
+    @objc func fetchJSON() {
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=10"
+        }
+        else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
+            }
+        }
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+
+    @objc func parse(json: Data) {
+        let decoder = JSONDecoder()
+
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        }
+        else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        }
+    }
+
+    @objc func showError() {
+        let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = petitions[indexPath.row].title
+        cell.detailTextLabel?.text = petitions[indexPath.row].body
+        return cell
+    }
+
+
+}
+
+
