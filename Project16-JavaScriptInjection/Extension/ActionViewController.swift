ece1bb5dd8468f931408734cf579fb33d461416d
kush <ryankush131@gmail.com>
2019-01-21 21:25:22 -0600

    Complete Project 16
    Completed project 16, which was a foray into Safari extensions
    and JavaScript injection. It was kind of a weird project, but I
    definately want to go back and do the challenges since they
    invlove using User Defaults and I haven’t worked much with
    them. I probably wouldn’t do the TableView challenge because
    it is a little too involved and I want to move on.

---
+++ Project16-JavaScriptInjection/Extension/ActionViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Kush, Ryan on 1/21/19.
+//  Copyright © 2019 Kush, Ryan. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var script: UITextView!
+
+    var pageTitle = ""
+    var pageURL = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+
+        if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [unowned self] (dict, error) in
+                    let itemDictionary = dict as! NSDictionary
+                    let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+                    self.pageTitle = javaScriptValues["title"] as! String
+                    self.pageURL = javaScriptValues["URL"] as! String
+
+                    DispatchQueue.main.async {
+                        self.title = self.pageTitle
+                    }
+
+                }
+            }
+        }
+    }
+
+    @objc func adjustForKeyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
+
+        let keyboardScreenEndFrome = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrome, from: view.window)
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = UIEdgeInsets.zero
+        }
+        else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        script.scrollIndicatorInsets = script.contentInset
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+
+        item.attachments = [customJavaScript]
+
+        self.extensionContext!.completeRequest(returningItems: [item])
+    }
+
+}
---
+++ Project16-JavaScriptInjection/Project16-JavaScriptInjection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project16-JavaScriptInjection
+//
+//  Created by Kush, Ryan on 1/21/19.
+//  Copyright © 2019 Kush, Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
