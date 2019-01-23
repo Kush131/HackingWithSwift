a281b296db65d47e2ba33d68910f9bfd96a4e7de
kush <ryankush131@gmail.com>
2019-01-22 21:37:01 -0600

    Commit part of Project 17
    Project 17 is a pretty long SpriteKit project, so I am going to
    push most of the initial setup code up and handle the rest of
    the project when I have access to two monitors at work (since
    I will need my iPad for testing instead of displaying the eBook).

---
+++ Project17-SwiftyNinja/Project17-SwiftyNinja/GameScene.swift
@@ -0,0 +1,127 @@
+//
+//  GameScene.swift
+//  Project17-SwiftyNinja
+//
+//  Created by Kush, Ryan on 1/22/19.
+//  Copyright © 2019 Kush, Ryan. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+
+        createScore()
+        createLives()
+        createSlices()
+    }
+
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+
+        addChild(gameScore)
+
+        gameScore.position = CGPoint(x: 8, y: 8)
+    }
+
+    func createLives() {
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            addChild(spriteNode)
+            livesImages.append(spriteNode)
+        }
+    }
+
+    func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 2
+
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            self.addChild(n)
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            self.addChild(n)
+        }
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
