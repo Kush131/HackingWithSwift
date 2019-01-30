414065c80ba3343228bad7d03b15eb5d83e70883
kush <ryankush131@gmail.com>
2019-01-29 22:29:10 -0600

    Commit Project 21
    Completed Project 21 which revolved around local push
    notifications. This is a really interesting area since I have seen
    things like this discussed with the Apollo Reddit app and how
    push notifications cost money (server processing fees) versus
    the “less often but on device” local notifications. While it was
    a really basic implementation, it was cool to get oriented with
    UNUserNotificationCenter and all of the various methods.

---
+++ Project21-LocalNotifications/Project21-LocalNotifications/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  Project21-LocalNotifications
+//
+//  Created by Kush, Ryan on 1/29/19.
+//  Copyright © 2019 Kush, Ryan. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+    }
+
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("yep")
+            }
+            else {
+                print("nope")
+            }
+        }
+    }
+
+    @objc func scheduleLocal() {
+        registerCategories()
+        let center = UNUserNotificationCenter.current()
+
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData" : "fizzbuzz"]
+        content.sound = UNNotificationSound.default
+
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+
+        center.setNotificationCategories([category])
+    }
+
+}
+
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                // User swiped to unlock
+                print("Default indiciator")
+            case "show":
+                // The user tapped our "show more info" button
+                print("Show more info")
+            default:
+                break
+            }
+        }
+        completionHandler()
+    }
+}
