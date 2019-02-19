73b19ab67f484d3fb2791fb3d5031d95629e12a8
kush <ryankush131@gmail.com>
2019-02-18 23:49:31 -0600

    Commit UI of Project 31
    Commited the Storyboard UI for Project 31. I didn’t have a ton of time to do any Hacking
    With Swift today, but I wanted to make sure to do a little bit of work. I have used
    StackViews a ton during my iOS development career, but learning about the usefulness
    of the “pin” menu where I can pin to neighbors was a revelation. I plan on learning more
    from “Modern AutoLayout”, but hopefully the rest of this chapter will be good for some
    other tidbits I had not picked up in my use of StackViews so far.

---
+++ Project31-Multibrowser/Project31-Multibrowser/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Project31-Multibrowser
+//
+//  Created by Kush, Ryan on 2/18/19.
+//  Copyright © 2019 Kush, Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
