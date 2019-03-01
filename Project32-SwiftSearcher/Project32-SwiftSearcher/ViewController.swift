b606967be0fd34df451cbae3bb07f096aa363834
kush <ryankush131@gmail.com>
2019-02-28 22:54:22 -0600

    Finish Project 32
    Finally finished Project 32, which was mostly about spotlight and how to index and
    deindex with it. It was really cool to once again see how simple it was to add a really
    cool feature to your app (and one that I use all the time with Spotlight). I also learned
    about SFSafariViewController, which I wish a lot more apps would utilize since it uses
    content blocking (unlike WKWebView).

--- Project32-SwiftSearcher/Project32-SwiftSearcher/ViewController.swift
+++ Project32-SwiftSearcher/Project32-SwiftSearcher/ViewController.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+import SafariServices
+import MobileCoreServices
+import CoreSpotlight
 
 class ViewController: UITableViewController {
 
     var projects = [[String]]()
+    var favorites = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true
 
         projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
         projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
@@ -23,6 +30,11 @@ class ViewController: UITableViewController {
         projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
         projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
         projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
+
+        let defaults = UserDefaults.standard
+        if let savedFavorites = defaults.object(forKey: "favorites") as? [Int] {
+            favorites = savedFavorites
+        }
     }
 
     func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
@@ -37,6 +49,16 @@ class ViewController: UITableViewController {
         return titleString
     }
 
+    func showTutorial(_ which: Int) {
+        if let url = URL(string: "https://www.hackingwithswift.com/read/\(which + 1)") {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+
+            let vc = SFSafariViewController(url: url, configuration: config)
+            present(vc, animated: true)
+        }
+    }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return projects.count
     }
@@ -44,10 +66,77 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
+        if favorites.contains(indexPath.row) {
+            cell.editingAccessoryType = .checkmark
+        }
+        else {
+            cell.editingAccessoryType = .none
+        }
+
         let project = projects[indexPath.row]
         cell.textLabel?.attributedText = makeAttributedString(title: project[0], subtitle: project[1])
 
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if favorites.contains(indexPath.row) {
+            return .delete
+        }
+        else {
+            return .insert
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .insert {
+            favorites.append(indexPath.row)
+            index(item: indexPath.row)
+        }
+        else {
+            if let index = favorites.firstIndex(of: indexPath.row) {
+                favorites.remove(at: index)
+                deindex(item: indexPath.row)
+            }
+        }
+
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
+
+    func index(item: Int) {
+        let project = projects[item]
+
+        let attributedSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeText as String)
+        attributedSet.title = project[0]
+        attributedSet.contentDescription = project[1]
+
+        let item = CSSearchableItem(uniqueIdentifier: "\(item)", domainIdentifier: "com.hackingwithswift", attributeSet: attributedSet)
+        item.expirationDate = Date.distantFuture
+        CSSearchableIndex.default().indexSearchableItems([item]) { error in
+            if let error = error {
+                print("Indexing error: \(error.localizedDescription)")
+            }
+            else {
+                print("Search item successfully indexed!")
+            }
+        }
+    }
+
+    func deindex(item: Int) {
+        CSSearchableIndex.default().deleteSearchableItems(withIdentifiers: ["\(item)"]) { error in
+            if let error = error {
+                print("Deindexing error: \(error.localizedDescription)")
+            } else {
+                print("Search item successfully removed!")
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showTutorial(indexPath.row)
     }
 }
 
